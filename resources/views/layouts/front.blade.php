8a13658796bc65dcb47105e709f8d5ee152c2098
arindra97 <ar28297@gmail.com>
2022-04-18 09:42:45 +0700

    slicing front 10%

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,4 +1,28 @@
 <!doctype html>
 <html x-data="data()" lang="en">
+    <head>
+        @include('includes.Landing.meta')
 
+        <title>@yield('title') | Freelancer</title>
+
+        @stack('before-style')
+            @include('includes.Landing.style')
+        @stack('after-style')
+    </head>
+
+    <body class="antialiased">
+        <div class="relative">
+            <!-- top -->
+            <div class="hero-bg">
+                <!-- header -->
+                @include('includes.Landing.header')
+                    @yield('content')
+                @include('includes.Landing.footer')
+
+                @stack('before-script')
+                    @include('includes.Landing.script')
+                @stack('after-script')
+            </div>
+        </div>
+    </body>
 </html>
